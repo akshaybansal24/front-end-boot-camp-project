447328b5a882b4dab25f24c5b7eabf62b027c9db
Akshay Bansal <akshay_bansal@intuit.com>
2021-12-09 13:04:55 -0700

    Fixing Table for Election View and Adding table for Election Results

--- TiggerVotingSystem/src/components/ElectionList.js
+++ TiggerVotingSystem/src/components/ElectionList.js
@@ -6,16 +6,27 @@ export const ElectionList = props => {
             <h1 style={{fontSize: "1.5rem"}}>List of Elections</h1>
             <table>
             <thead>
-
+                <tr>
+                    <th>Election Name</th>
+                    <th>Election Year</th>
+                    <th>View Results</th>
+                </tr>
             </thead>
             <tbody>
             { isEmpty(props.elections)
                     ? <td>No Elections</td> :
                     props.elections.map(election => {
                             return(
-                                <td key={election.id}>{election.name}&emsp;{election.year}&emsp;
-                                    <button type="button" onClick={() => props.onViewElectionResults(election)}>View Results</button>
-                                </td>
+                                <tr key={election.id}>
+                                    <td>{election.name}</td>
+                                    <td>{election.year}</td>
+                                    <td>
+                                        <button type="button" onClick={() => props.onViewElectionResults(election)}>View Results</button>
+                                    </td>
+                                </tr>
+                                // <td >{election.name}&emsp;{election.year}&emsp;
+                                //     <button type="button" onClick={() => props.onViewElectionResults(election)}>View Results</button>
+                                // </td>
                             );
                         }
                     
--- TiggerVotingSystem/src/components/ElectionResults.js
+++ TiggerVotingSystem/src/components/ElectionResults.js
@@ -3,11 +3,26 @@ export const ElectionResults = props => {
     return(
         <section>
             <h1 style={{fontSize: "1.5rem"}}>Result of Election {props.election.name}</h1>
-            <ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Question</th>
+                        <th>Yes Count</th>
+                        <th>No Count</th>
+                    </tr>
+                </thead>
+                <tbody>
                 {
-                    props.election.questions.map(question => { return (<li key={question.id}>{question.question} &ensp;Yes: {question.yes}  No: {question.no} </li>) } )
+                    props.election.questions.map(question => { return (
+                        <tr key={question.id}>
+                            <td>{question.question}</td>
+                            <td>{question.yes}</td>
+                            <td>{question.no}</td>
+                        </tr>
+                    ) } )
                 }
-            </ul>
+                </tbody>
+            </table>
         </section>
     );
 };
